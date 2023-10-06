7e7e09fa14e701bd524150a339608d3c775c3984
tarasusrus <bar.norilsk@gmail.com>
2023-10-06 18:39:54 +0300

    Initial commit

---
+++ board/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Bb
+
+
+class BbForm(ModelForm):
+    class Meta:
+        model = Bb
+        fields = (
+            'title',
+            'content',
+            'prise',
+            'rubric'
+        )
